ac2f7a3e355b0f9e73c2d8bb53ae54495e624722
ducky1129 <schober1129@gmail.com>
2024-10-30 12:57:24 -0500

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,80 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+interface Ingredient{
+    String getName();
+    float getQuantity();
+}
+class SolidIngredient implements Ingredient{
+    private String name;
+    private float quantity;
+    public SolidIngredient(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+    public String getName(){
+        return name;
+    }
+    public float getQuantity(){
+        return quantity;
+    }
+    public String toString(){
+        return name + ": " + quantity;
+    }
+}
+
+class LiquidIngredient implements Ingredient{
+    private String name;
+    private float quantity;
+    public LiquidIngredient(String name, int quantity){
+        this.name = name;
+        this.quantity = quantity;
+    }
+    public String getName(){
+        return name;
+    }
+    public float getQuantity(){
+        return quantity;
+    }
+    public String toString(){
+        return name + ": " + quantity;
+    }
+}
+
+class Recipe<T extends Ingredient> {
+    private ArrayList<T> ingredients;
+    private String name;
+    public Recipe(String name){
+        this.name = name;
+        ingredients = new ArrayList<>();
+    }
+    public Recipe(String name, ArrayList<T> ingredients){
+        this.name = name;
+        this.ingredients = ingredients;
+    }
+
+    public void addIngredient(T ingredient){
+        ingredients.add(ingredient);
+    }
+
+    public void print(){
+        System.out.println("Recipe for " + name);
+        for (T ingredient : ingredients){
+            System.out.println(ingredient);
         }
+    }
+}
+public class Main {
+
+    public static void menu(){
+
+    }
+    public static void main(String[] args) {
+        Recipe<Ingredient> r = new Recipe<>("Cake");
+        Ingredient si1 = new LiquidIngredient("Milk", 20);
+        Ingredient si2 = new SolidIngredient("Egg", 2);
+        r.addIngredient(si1);
+        r.addIngredient(si2);
+        r.print();
     }
 }
